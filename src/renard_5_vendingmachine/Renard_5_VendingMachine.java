d6751a74a455b37ee51378ed1dc9a3e291113627
root <root@ninja13.d22.lan>
2017-05-10 13:39:22 -0500

    first commit

---
+++ src/renard_5_vendingmachine/Renard_5_VendingMachine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package renard_5_vendingmachine;
+
+/**
+ *
+ * @author root
+ */
+public class Renard_5_VendingMachine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
